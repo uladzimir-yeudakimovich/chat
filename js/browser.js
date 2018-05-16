6df32339205ff220800a8205bb0e9b2da4539149
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-05-16 16:36:52 +0300

    updated forms

--- js/browser.js
+++ js/browser.js
@@ -5,17 +5,58 @@ if (!window.WebSocket) {
 // create a connection
 const socket = new WebSocket("ws://localhost:8081");
 
+document.forms.registration.onsubmit = function() {
+  let username = this.username.value;
+  let password = this.psw.value;
+  let email    = this.email.value;
+  let data     = {
+    message: "registration",
+    user   : {
+      username: username,
+      password: password,
+      email   : email
+    }
+  };
+  socket.send(JSON.stringify(data));
+  return false;
+};
+
+document.forms.login.onsubmit = function() {
+  let username = this.login.value;
+  let password = this.password.value;
+  let data = {
+    message: "login",
+    user   : {
+      username: username,
+      password: password
+    }
+  };
+  socket.send(JSON.stringify(data));
+  return false;
+};
+
 // send a message from the form publish
 document.forms.publish.onsubmit = function() {
   let outgoingMessage = this.message.value;
-  socket.send(outgoingMessage);
+  let data = {
+    message: "message",
+    user   : {
+      uid  : uid,
+      value: outgoingMessage
+    }
+  };
+  socket.send(JSON.stringify(data));
   return false;
 };
 
 // handler of inbound messages
 socket.onmessage = function(event) {
-  let incomingMessage = event.data;
-  showMessage(incomingMessage);
+  let incomingMessage = JSON.parse(event.data);
+  // if(incomingMessage.message === "registration") {}
+  // if(incomingMessage.message === "login") {}
+  if(incomingMessage.message === "message") {
+    showMessage(incomingMessage.user.value);
+  }
 };
 
 // show the message in div#subscribe
